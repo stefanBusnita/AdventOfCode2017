f5f10ffa7165a79181c0cf71b2fc1e710778baeb
Stefan Busnita <stefanbusnita@yahoo.com>
2018-06-24 18:00:27 +0200

    Fix on cursor movement

--- Day25-Touring-Machine/main.go
+++ Day25-Touring-Machine/main.go
@@ -40,7 +40,7 @@ type Tape struct {
 
 func NewTape(size int) *Tape {
 	return &Tape{
-		content: make([]int, size),
+		content: make([]int, size*10),
 		states:  make(map[string]*State, 0),
 	}
 }
@@ -49,39 +49,56 @@ func (t *Tape) addState(key string, state *State) {
 	t.states[key] = state
 }
 
-func (t *Tape) start(steps int, startStateKey string) {
+func (t *Tape) start(steps int, key string) {
 
 	cursor := Cursor(steps * 10 / 2)
-	fmt.Printf("Cursor start value %d \n", cursor)
+	fmt.Printf("Cursor start value %d and state %s \n", cursor, key)
 
 	iterations := 0
 	var writeVal WriteVal
+	var newCursor Cursor
 
 	for iterations < steps {
 
-		state := t.states[startStateKey]
+		state := t.states[key]
 		currentTapeValue := t.content[cursor]
 
-		// call do effect which should modify the tape
-		startStateKey = state.config[currentTapeValue].nextStateKey
+		fmt.Printf("State %s, Cursor %d,current value %d, next value %d \n", key, cursor, currentTapeValue, int(writeVal))
+		newCursor, writeVal = state.config[currentTapeValue].effect(cursor)
 
-		cursor, writeVal = state.config[currentTapeValue].effect(cursor)
-		//fmt.Printf("Current cursor %d with the next value %d \n", cursor, int(writeVal))
+		fmt.Printf("State %s, Cursor %d,current value %d, next value %d \n \n\n\n", key, cursor, currentTapeValue, int(writeVal))
+		// assign new value
 		t.content[int(cursor)] = int(writeVal)
-
-		// just for testing, the sum of all these ones is the checksum
-		if t.content[int(cursor)] == 1 {
-			fmt.Println("One One")
-		}
-		//fmt.Printf("How does the tape look now ? %+v \n", t.content)
-
+		// get the next state key
+		key = state.config[currentTapeValue].nextStateKey
+		// move cursor ahead
+		cursor = newCursor
+		// count the next iteration
 		iterations++
 	}
+	onez2 := 0
+	for i := 0; i < len(t.content); i++ {
+		if t.content[i] == 1 {
+			onez2++
+		}
+	}
+
+	fmt.Printf("Final count %d \n", onez2)
 
 }
 
 func main() {
-	tape := NewTape(50)
+	tape := NewTape(12794428)
+
+	/*	In state A:
+		If the current value is 0:
+		  - Write the value 1.
+		  - Move one slot to the right.
+		  - Continue with state B.
+		If the current value is 1:
+		  - Write the value 0.
+		  - Move one slot to the left.
+		  - Continue with state F.*/
 
 	// create and describe stateA
 	stateA := NewState(tape)
@@ -92,20 +109,134 @@ func main() {
 		},
 	})
 	stateA.addEffect(1, &Effect{
-		nextStateKey: "B",
+		nextStateKey: "F",
 		effect: func(cursor Cursor) (Cursor, WriteVal) {
 			return cursor - 1, ZERO
 		},
 	})
+
+	/*
+			In state B:
+		  If the current value is 0:
+		    - Write the value 0.
+		    - Move one slot to the right.
+		    - Continue with state C.
+		  If the current value is 1:
+		    - Write the value 0.
+		    - Move one slot to the right.
+		    - Continue with state D.
+	*/
 	// create and describe stateB
 	stateB := NewState(tape)
 	stateB.addEffect(0, &Effect{
+		nextStateKey: "C",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor + 1, ZERO
+		},
+	})
+	stateB.addEffect(1, &Effect{
+		nextStateKey: "D",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor + 1, ZERO
+		},
+	})
+
+	/*In state C:
+	  If the current value is 0:
+	    - Write the value 1.
+	    - Move one slot to the left.
+	    - Continue with state D.
+	  If the current value is 1:
+	    - Write the value 1.
+	    - Move one slot to the right.
+	    - Continue with state E.*/
+	stateC := NewState(tape)
+	stateC.addEffect(0, &Effect{
+		nextStateKey: "D",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor - 1, ONE
+		},
+	})
+	stateC.addEffect(1, &Effect{
+		nextStateKey: "E",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor + 1, ONE
+		},
+	})
+	/*
+
+		In state D:
+		  If the current value is 0:
+		    - Write the value 0.
+		    - Move one slot to the left.
+		    - Continue with state E.
+		  If the current value is 1:
+		    - Write the value 0.
+		    - Move one slot to the left.
+		    - Continue with state D.
+	*/
+	stateD := NewState(tape)
+	stateD.addEffect(0, &Effect{
+		nextStateKey: "E",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor - 1, ZERO
+		},
+	})
+	stateD.addEffect(1, &Effect{
+		nextStateKey: "D",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor - 1, ZERO
+		},
+	})
+
+	/*
+
+		In state E:
+		  If the current value is 0:
+		    - Write the value 0.
+		    - Move one slot to the right.
+		    - Continue with state A.
+		  If the current value is 1:
+		    - Write the value 1.
+		    - Move one slot to the right.
+		    - Continue with state C.
+	*/
+
+	stateE := NewState(tape)
+	stateE.addEffect(0, &Effect{
+		nextStateKey: "A",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor + 1, ZERO
+		},
+	})
+	stateE.addEffect(1, &Effect{
+		nextStateKey: "C",
+		effect: func(cursor Cursor) (Cursor, WriteVal) {
+			return cursor + 1, ONE
+		},
+	})
+
+	/*
+		In state F:
+		  If the current value is 0:
+		    - Write the value 1.
+		    - Move one slot to the left.
+		    - Continue with state A.
+		  If the current value is 1:
+		    - Write the value 1.
+		    - Move one slot to the right.
+			- Continue with state A.
+
+	*/
+
+	stateF := NewState(tape)
+	stateF.addEffect(0, &Effect{
 		nextStateKey: "A",
 		effect: func(cursor Cursor) (Cursor, WriteVal) {
 			return cursor - 1, ONE
 		},
 	})
-	stateB.addEffect(1, &Effect{
+	stateF.addEffect(1, &Effect{
 		nextStateKey: "A",
 		effect: func(cursor Cursor) (Cursor, WriteVal) {
 			return cursor + 1, ONE
@@ -114,7 +245,15 @@ func main() {
 
 	tape.addState("A", stateA)
 	tape.addState("B", stateB)
+	tape.addState("C", stateC)
+	tape.addState("D", stateD)
+	tape.addState("E", stateE)
+	tape.addState("F", stateF)
 
-	tape.start(5, "A")
+	tape.start(12794428, "A")
+
+	// 3198607 Wrong !
+
+	// 3198607 MIGHT BE GOOD, WE CAN TRY IN ONE MINUTE
 
 }
